19de43599a98583e40d9bd9a4426c2d34d566d7d
amartineztapia <103907515+amartineztapia@users.noreply.github.com>
2022-04-19 15:27:48 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -14,8 +14,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
 
-dr = r'C:\Users\alber\Documents\Spring 22\AI&ML\loan-train.csv'
-Data = pd.read_csv(dr)
+
+Data = pd.read_csv('loan-train.csv')
+
+
+primaryColor="#F63366"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="sans serif"
 
 ## The model
 df = Data
@@ -29,14 +36,26 @@ y = df['Loan_Status']
 (X_train, X_test,
  y_train, y_test) = train_test_split(X, y, test_size=0.25)
 
-## Bunch of sliders
+## Inputs
 
 col_names = df.columns
-Drop_Gender = ['Loan_Status']
-col_names = col_names.drop(Drop_Gender)
+col_names = col_names.drop(['Loan_Status'])
+col_names2 = col_names.drop([ "Married", "Self_Employed", "Credit_History", "Gender_Female"])
+col_names3 = col_names.drop(col_names2)
 
-for i in col_names:
+Binary_options = {
+    0: "No",
+    1: "Yes",
+    }
+
+for i in col_names3:
+    globals()[f'{i}'] = st.radio(i, (0, 1), index = int(df[i].median()), format_func=lambda x: Binary_options.get(x))
+    
+for i in col_names2:
     globals()[f'{i}'] = st.slider(i, int(df[i].min()), int(df[i].max()), int(df[i].median()))
+
+Gender_Female = st.radio("You gay?", (0, 1), index = 0, format_func=lambda x: Binary_options.get(x))
+
 
 
 ## The regression
@@ -54,8 +73,6 @@ if pred < 50:
 else:
     st.metric(label="Probability of Acceptance", value=str(pred)+'%', delta="Accepted")
     st.balloons()
-
-
 
 
 
